6c10ae402611df0b2f67b56ececfa6652fba15a9
Yuan-Ming Hsu <48866415+technic960183@users.noreply.github.com>
2026-06-21 23:10:25 -0700

    Align make setting with Athena++
    Change to HLLD

--- example/test_problem/Hydro/CR_Streaming_Blast_Stream/generate_make.sh
+++ example/test_problem/Hydro/CR_Streaming_Blast_Stream/generate_make.sh
@@ -2,5 +2,5 @@
 
 PYTHON=python3
 
-${PYTHON} configure.py --model=HYDRO --flu_scheme=MHM_RP --flux=HLLE --mhd=true --slope=PLM \
+${PYTHON} configure.py --model=HYDRO --flu_scheme=MHM_RP --flux=HLLD --mhd=true --slope=PLM \
                        --cr_streaming=true --eos=GAMMA --hdf5=true --openmp=true --double=true "$@"

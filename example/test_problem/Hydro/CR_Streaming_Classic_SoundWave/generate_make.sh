552d5f4616e01d4b81da1969ff6bb54643a81933
Yuan-Ming Hsu <48866415+technic960183@users.noreply.github.com>
2026-07-04 08:59:26 -0700

    Update input files and add classic test problems

---
+++ example/test_problem/Hydro/CR_Streaming_Classic_SoundWave/generate_make.sh
@@ -0,0 +1,6 @@
+# This script should run in the same directory as configure.py
+
+PYTHON=python3
+
+${PYTHON} configure.py --model=HYDRO --flu_scheme=MHM_RP --flux=HLLD --mhd=true --slope=PLM \
+                       --cr_streaming=true --eos=GAMMA --hdf5=true --openmp=true --double=true "$@"
